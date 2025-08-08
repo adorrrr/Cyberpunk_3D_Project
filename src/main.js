baa7a26d2189f2488f698b2cdf5c098ca116881c
Ahasanul Monir Adar <ahasanulmoniradar@gmail.com>
2025-08-09 00:51:38 +0600

    push

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { While } from 'three/examples/jsm/transpiler/AST.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css';
 import * as THREE from 'three';
 
@@ -7,12 +9,6 @@ const scene = new THREE.Scene();
 //camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 camera.position.z = 4;
-//object
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 'whte' });    
-const cube = new THREE.Mesh(geometry, material);    
-scene.add(cube);
-
 
 //renderer
 const renderer = new THREE.WebGLRenderer({
@@ -21,6 +17,35 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
+
+new RGBELoader().load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/pond_bridge_night_1k.hdr', function(texture){
+    const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+    scene.environment = envMap;
+    texture.dispose();
+    pmremGenerator.dispose();
+
+    const loader = new GLTFLoader();
+    loader.load('public/DamagedHelmet.gltf', function(gltf){
+        scene.add(gltf.scene);
+    },undefined,function(error){
+        console.error('An error happened',error);
+    });
+});
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    // controls.dampingFactor = 0.05;
+    controls.enableZoom = true;
+    controls.enablePan = true;
+    controls.enableRotate = true;
+    controls.update();
+
 
 
 function animate(){
